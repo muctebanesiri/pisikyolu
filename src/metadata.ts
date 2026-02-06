c497db914706633304e2f3b6b202b23ec7075808
Mücteba Nesiri <muctebanesiri@gmail.com>
2026-02-06 08:05:02 +0330

    عحیشفث

--- src/metadata.ts
+++ src/metadata.ts
@@ -4,9 +4,10 @@ export const hisMetadata = {
     "What are the parallels between faith and open source software? Join Nadia and Henry for an off-the-cuff conversation between friends.",
   gitOrg: "muctebanesiri",
   gitRepo: "",
-  siteUrl: "https://pisikyolu.com",
+  siteUrl: "https://fm.mucteba.ir",
   twitter: "@muctebanesiri",
   rss: "https://rss.castbox.fm/everest/49d984cb4b34479d92e292828475329f.xml",
-  youtube: "https://www.youtube.com/@muctebanesiri",
+  youtube: "yt.mucteba.ir",
   apple: "https://podcasts.apple.com/by/podcast/pi%C5%9Fikyolu-%D9%BE%D9%8A%D8%B4%D9%8A%DA%A9-%D9%8A%D9%88%D9%84%D9%88/id1777842418",
+  spotify: "https://www.youtube.com/@muctebanesiri",
 } as const;
