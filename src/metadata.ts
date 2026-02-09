130b1a2453fd7338b35e372658934b3752ad4b09
Mücteba Nesiri <muctebanesiri@gmail.com>
2026-02-10 00:56:29 +0330

    add spotify link

--- src/metadata.ts
+++ src/metadata.ts
@@ -9,5 +9,5 @@ export const hisMetadata = {
   rss: "https://rss.castbox.fm/everest/49d984cb4b34479d92e292828475329f.xml",
   YouTube: "https://yt.mucteba.ir",
   apple: "https://podcasts.apple.com/by/podcast/pi%C5%9Fikyolu-%D9%BE%D9%8A%D8%B4%D9%8A%DA%A9-%D9%8A%D9%88%D9%84%D9%88/id1777842418",
-  spotify: "https://www.YouTube.com/@muctebanesiri",
+  spotify: "https://open.spotify.com/show/0Ar45UeKDqqVlzqJf4NyIz",
 } as const;
