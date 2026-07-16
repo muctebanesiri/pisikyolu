985229a37370a30971789df52f74c513f607f8e9
Mücteba Nesiri <git@mucteba.ir>
2026-07-16 07:46:24 +0300

    Update metadata.ts

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,13 +1,13 @@
 export const hisMetadata = {
-  title: "Hope in Source",
+  title: "پیشیک‌یولو",
   description:
-    "What are the parallels between faith and open source software? Join Nadia and Henry for an off-the-cuff conversation between friends.",
-  gitOrg: "hzoo",
-  gitRepo: "hopeinsource.com",
-  siteUrl: "https://hopeinsource.com",
-  twitter: "@left_pad",
-  rss: "https://anchor.fm/s/ff707650/podcast/rss",
-  youtube: "https://www.youtube.com/@hopeinsource",
-  apple: "https://itunes.apple.com/us/podcast/hope-in-source/id1437677655",
-  spotify: "https://open.spotify.com/show/5EXwiyKzPrrucncKyrHy0B",
+    "پیشیک‌یولو، مجتبی نصیری طرفیندن، حقوق، ادبیات، تکنولوژی و بنزری موضوعاتا گوره بیر پادکستدیر.",
+  gitOrg: "muctebanesiri",
+  gitRepo: "",
+  siteUrl: "https://fm.mucteba.ir",
+  twitter: "@muctebanesiri",
+  rss: "https://rss.castbox.fm/everest/49d984cb4b34479d92e292828475329f.xml",
+  YouTube: "https://yt.mucteba.ir",
+  apple: "https://podcasts.apple.com/by/podcast/pi%C5%9Fikyolu-%D9%BE%D9%8A%D8%B4%D9%8A%DA%A9-%D9%8A%D9%88%D9%84%D9%88/id1777842418",
+  spotify: "https://open.spotify.com/show/0Ar45UeKDqqVlzqJf4NyIz",
 } as const;
